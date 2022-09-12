6ec7fff01ceecea72d62053d631c721a19036907
Deri Eszter <deri.eszti24@gmail.com>
2022-09-12 19:52:23 +0200

    hibák kijavitva

--- main.cpp
+++ main.cpp
@@ -2,25 +2,21 @@
 
 constexpr int N_ELEMENTS = 100;
 
-int main()
-{
-    int *b = new int[NELEMENTS];//*N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'//;
-    for (int i = 0;)//for kiegeszitese
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i; i++)//for feltétel kiegészítése
-    {
-        std::cout << "Ertek:"//ki kellene írni az értéket is
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)//;
-    {
-        atlag += b[i]
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+int main() {
+  int *b = new int[N_ELEMENTS];
+  std::cout << "1-100 ertekek duplazasa";
+  for (int i = 0; i < N_ELEMENTS; i++) {
+    b[i] = i * 2;
+  }
+  for (int i = 0; i < N_ELEMENTS; i++) {
+    std::cout << "Ertek:" << b[i] <<std::endl;
+  }
+  std::cout << "Atlag szamitasa: " << std::endl;
+  int atlag;
+  for (int i = 0; i < N_ELEMENTS; i++) {
+    atlag += b[i];
+  }
+  atlag /= N_ELEMENTS;
+  std::cout << "Atlag: " << atlag << std::endl;
+  return 0;
 }
